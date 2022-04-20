15363f9571d37cc38ea94fdbb0e85ad1cf53f09d
jzx <945627077@qq.com>
2022-04-20 13:07:46 +0800

    '增加测试组件'

---
+++ src/node/Container.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        容器
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name:"Container"
+})
+</script>
+
+
+
---
+++ src/node/Text.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{content}}
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name:"Text",
+    props:{
+        content:{
+            type:String,
+            default:"显示的文字"
+        }
+    }
+})
+</script>
+
+
+
