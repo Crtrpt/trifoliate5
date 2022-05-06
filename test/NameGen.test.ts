52468aed1d15922e3953e19f6351efe5e1e718e3
jzx <945627077@qq.com>
2022-05-07 01:44:45 +0800

    修改测试用例错误

--- test/NameGen.test.ts
+++ test/NameGen.test.ts
@@ -1,7 +1,5 @@
 import { nameGen } from "../src/utils/nameGen";
 test(" test nameGen", () => {
   var name = "name";
-  nameGen(name);
-
-  expect(name).toBe("name-1");
+  expect(nameGen(name)).toBe("name-1");
 });
